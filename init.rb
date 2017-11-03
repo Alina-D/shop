fa02aa67215f2a16f014317a3013ed8dd1718459
Alina-D <TwinklingStar1488@gmail.com>
2017-11-03 22:20:32 +0300

    Проверяем работу с помощью файла init

---
+++ init.rb
@@ -0,0 +1,20 @@
+require_relative 'item_container'
+require_relative 'cart'
+require_relative 'order'
+require_relative 'item'
+require_relative 'virtual_item'
+require_relative 'real_item'
+
+@items =[]
+@items << VirtualItem.new({ :price => 100, :name => 'car'})
+@items << VirtualItem.new({ :price => 101, :name => 'kettle'})
+@items << RealItem.new({ :price => 101, :weight => 10, :name => 'fridge'})
+
+
+
+
+
+
+
+
+
